be9b37990d817b387fab34af97f05e0b01b1a3c0
Patrick de Boer <pdeboer@mit.edu>
2015-09-17 13:08:47 +0200

    more efficient

--- src/main/scala/ch/uzh/ifi/pdeboer/pplib/hcomp/ballot/integrationtest/console/ConsoleIntegrationTest.scala
+++ src/main/scala/ch/uzh/ifi/pdeboer/pplib/hcomp/ballot/integrationtest/console/ConsoleIntegrationTest.scala
@@ -29,13 +29,13 @@ object ConsoleIntegrationTest extends App with LazyLogger {
 
 	val ballotPortalAdapter = HComp(BallotPortalAdapter.PORTAL_KEY)
 
-  /**
-   * Algorithm 250:
-   * The algorithm250 allows to disable redundant questions (or permutations).
-   * First, all permutations with the same groupname are disabled since an assumption is assumed to be related only to a single method.
-   * Second, all the permutation with the same methodIndex, pdf filename and assumption name are disabled. This second step is performed
-   * because it is assumed that a method (or group of methods) is/are related to only one assumption type.
-   */
+	/**
+	 * Algorithm 250:
+	 * The algorithm250 allows to disable redundant questions (or permutations).
+	 * First, all permutations with the same groupname are disabled since an assumption is assumed to be related only to a single method.
+	 * Second, all the permutation with the same methodIndex, pdf filename and assumption name are disabled. This second step is performed
+	 * because it is assumed that a method (or group of methods) is/are related to only one assumption type.
+	 */
 	val algorithm250 = Algorithm250(dao, ballotPortalAdapter)
 
 	val groups = dao.getAllPermutations().groupBy(gr => {
@@ -44,10 +44,10 @@ object ConsoleIntegrationTest extends App with LazyLogger {
 
 	groups.mpar.foreach(group => {
 		group._2.foreach(permutation => {
-      if(dao.getPermutationById(permutation.id).isDefined && dao.getPermutationById(permutation.id).get.state == 0){
-        algorithm250.executePermutation(permutation)
-      }
-    })
+			if (dao.getPermutationById(permutation.id).map(_.state).getOrElse(-1) == 0) {
+				algorithm250.executePermutation(permutation)
+			}
+		})
 	})
 
 	Report.writeCSVReport(dao) //TODO does this always export everything in the DB? I'd consider changing this such that .writeCSVReport gets a list of HCompAnswer's
